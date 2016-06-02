9d194aa85699a39db6cc2c334ab4578444f9a072
Rodolfo Dias <rmdias29@gmail.com>
2016-06-01 22:35:58 -0300

    change algorithm to TS

--- tic-tac-toe.ts
+++ tic-tac-toe.ts
@@ -1,7 +1,7 @@
-var hash = ['o','o','x','o','x','o','x','o','x'];
+var hash:string[] = ['o','o','x','o','x','o','x','o','x'];
 
-const checkGame = (hash) => {
-  let hashLength = hash.length;
+const checkGame = (hash:any) => {
+  let hashLength:Number = hash.length;
     if (hashLength >= 5){
       for (var i = 0; i < hash.length; i++) {
         if ((i === 1 || i === 4 || i === 7) && (hash[i] === hash[i-1]) && (hash[i] === hash[i+1])){
@@ -15,7 +15,7 @@ const checkGame = (hash) => {
         }else if((i === 0 || i === 2) && (hash[i] === hash[i+4]) && (hash[i] === hash[i+8] || hash[i] === hash[i+2])){
           // diagonal comparation
 
-        break;
+          break;
         }else{
 
         }
