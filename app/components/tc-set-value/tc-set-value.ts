7d05d66efbfe85ddaca1b64a3e9bff8cccba3a0b
Rodolfo Dias <rmdias29@gmail.com>
2016-06-08 10:57:37 -0300

    add set-value @Directive

---
+++ app/components/tc-set-value/tc-set-value.ts
@@ -0,0 +1,23 @@
+import {Directive, ElementRef, Input} from '@angular/core';
+
+@Directive({
+   selector : '[tc-set-value]',
+   inputs:['value'],
+   host: {
+    '(click)': 'writeMove()'
+   }
+})
+
+export class tcSetValue{
+  constructor(public _elementRef: ElementRef) {
+  }
+ 
+  value:string;
+
+  writeMove(){
+    if (this._elementRef.nativeElement.innerHTML.length < 1)
+      this._elementRef.nativeElement.innerHTML = this.value;    
+  }
+}
+
+
