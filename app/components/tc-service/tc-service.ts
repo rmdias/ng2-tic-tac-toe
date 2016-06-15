06a530d82123ddd684e6fc9522e702d9dbc87480
Rodolfo Dias <rmdias29@gmail.com>
2016-06-14 21:43:25 -0300

    TS: Improve the winner search

--- app/components/tc-service/tc-service.ts
+++ app/components/tc-service/tc-service.ts
@@ -6,13 +6,13 @@ export class tcService{
   
   checkGame(hash:any){
     let hashLength:Number = hash.length;
-    
+
     for (var i = 0; i < hash.length; i++) {
       if ((i === 1 || i === 4 || i === 7) && (hash[i] === hash[i-1]) && (hash[i] === hash[i+1]))
         return this.winner(hash[i], i);
       else if((i === 0 || i === 1 || i === 2) && (hash[i] === hash[i+3]) && (hash[i] === hash[i+6]))
         return this.winner(hash[i], i);
-      else if((i === 0 || i === 2) && (hash[i] === hash[i+4]) && (hash[i] === hash[i+8] || hash[i] === hash[i+6]))
+      else if((i === 0 || i === 2) && (hash[i] === hash[4]) && (hash[i] === hash[8] || hash[i] === hash[6]))
         return this.winner(hash[i], i);
     };
     if(hash.every(elem => typeof(elem) == 'string'))
