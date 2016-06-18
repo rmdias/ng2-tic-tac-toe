6eea6b06cbb8836ce585ddd91d76892f34584994
Rodolfo Dias <rmdias29@gmail.com>
2016-06-18 19:57:19 -0300

    remove not used code from tc-set-value

--- app/components/tc-set-value/tc-set-value.ts
+++ app/components/tc-set-value/tc-set-value.ts
@@ -1,8 +1,8 @@
-import {Directive, ElementRef, Input, OnChanges, SimpleChange} from '@angular/core';
+import {Directive, ElementRef, Input} from '@angular/core';
 
 @Directive({
    selector : '[tc-set-value]',
-   inputs:['value', 'endGame'],
+   inputs:['value'],
    host: {
     '(click)': 'writeMove()'
    }
@@ -12,25 +12,10 @@ export class tcSetValue{
   constructor(public _elementRef: ElementRef) {
   }
  
- 
   value:string;
-  endGame:boolean;
 
   writeMove(){
     if (this._elementRef.nativeElement.innerHTML.length < 1)
       this._elementRef.nativeElement.innerHTML = this.value;
   }
-  
-  cleanBattlefield(){
-     this.endGame = false;
-     setTimeout(() => this._elementRef.nativeElement.innerHTML = '', 2000);
-  }
-  
-  ngOnChanges(changes:{[propName:string]: SimpleChange}){
-    let isEndGame:any = changes['endGame'];
-    if (isEndGame !== undefined && isEndGame.currentValue !== undefined && isEndGame.currentValue)
-      this.cleanBattlefield();
-   }
 }
-
-
