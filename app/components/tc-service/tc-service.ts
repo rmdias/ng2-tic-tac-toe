f00c6d76260c4fa23438a942f5612863aea4088f
Rodolfo Dias <rmdias29@gmail.com>
2016-06-22 23:13:00 -0300

    remove debug log

--- app/components/tc-service/tc-service.ts
+++ app/components/tc-service/tc-service.ts
@@ -8,6 +8,7 @@ export class tcService{
     const factor = hash.length;
     const checkForColMatch = (hash, x) => (hash[x][0] === hash[x][1]) && (hash[x][1] === hash[x][2]) && hash[x][2];
     const checkForRowMatch = (hash, x) => (hash[0][x] === hash[1][x]) && (hash[1][x] === hash[2][x]) && hash[2][x];
+    
     const checkForXMatch = (hash) => {
       const center = hash[1][1];
 
@@ -15,13 +16,12 @@ export class tcService{
         return false;
 
       return hash[0][0] === hash[2][2] && hash[2][2] === center || hash[0][2] === hash[2][0] && hash[2][0] === center;
-
     };
 
     for (let i = 0; i < factor; i += 1) {
-
-      if (checkForColMatch(hash, i) || checkForRowMatch(hash, i))
-          return {endGame: true};
+      if (checkForColMatch(hash, i) || checkForRowMatch(hash, i)){
+        return {endGame: true};        
+      }
     }
 
     if (checkForXMatch(hash))
